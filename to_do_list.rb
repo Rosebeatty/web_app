3987f9e9d4a1a036865e38e597a8112c663b063b
Rose Beazzy <rose.beatty@gmail.com>
2018-06-25 14:57:40 +0100

    added classes to to do list

--- to_do_list.rb
+++ to_do_list.rb
@@ -1,15 +1,53 @@
+class Item
+    
+    attr_reader :description
+    
+    def initialize(description)
+        @description = description
+        @completed = false
+    end
+    
+    def complete_task
+        @completed = true
+    end
+    
+end
+
+
+class ToDoList
+    
+    def initialize()
+       @to_do_list = []
+    end
+    
+    def add_task(item)
+       @to_do_list.push(item)
+    end
+    
+    def print_list
+        @to_do_list.each {|item| puts item.description}
+    end
+    
+    def remove_task(item)
+        @to_do_list.delete(item)
+    end
+        
+end
+
+
+
 add_item = true
-to_do_list = []
+to_do_list = ToDoList.new
 
 while add_item == true 
     puts "Add an item to your to do list"
-    item = gets.chomp
-    to_do_list.push(item) 
-    puts to_do_list
+    item = Item.new(gets.chomp)
+    to_do_list.add_task(item) 
+    to_do_list.print_list()
     puts "Do you want to add another item? Yes or no?"
     response = gets.chomp
     if response!="yes"
     add_item = false
     end
-    puts to_do_list
+    to_do_list.print_list()
 end
